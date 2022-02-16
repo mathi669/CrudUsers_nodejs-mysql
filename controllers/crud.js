3beaa5a8297d9107ef7294e90f502586cc4940d7
mathi669 <44332506+mathi669@users.noreply.github.com>
2022-02-16 18:16:38 -0300

    Update y Delete
    Metodos Update y delete

--- controllers/crud.js
+++ controllers/crud.js
@@ -1,13 +1,50 @@
-const conexion = require('../database/db')
+const conexion = require("../database/db");
 
-exports.save = (req, res)=>{
-   const user = req.body.user;
-   const rol = req.body.rol;
-   conexion.query('INSERT INTO users SET ?', {user:user, rol:rol}, (error, results)=>{
-       if(error){
-           console.log(error);
-       } else {
-           res.redirect('/');
-       }
-   })
-}
+exports.save = (req, res) => {
+  const user = req.body.user;
+  const rol = req.body.rol;
+  const edad = req.body.edad;
+  const email = req.body.email;
+  const salario = req.body.salario;
+
+  conexion.query(
+    "INSERT INTO users SET ?",
+    { user: user, rol: rol, edad: edad, email: email, salario: salario },
+    (error, results) => {
+      if (error) {
+        console.log(error);
+      } else {
+        res.redirect("/");
+      }
+    }
+  );
+};
+
+exports.update = (req, res) => {
+  const id = req.body.id;
+  const user = req.body.user;
+  const rol = req.body.rol;
+  const edad = req.body.edad;
+  const email = req.body.email;
+  const salario = req.body.salario;
+
+  conexion.query(
+    "UPDATE users SET ? WHERE id = ?",
+    [
+      {
+        user: user,
+        rol: rol,
+        edad: edad,
+        email: email,
+        salario: salario,
+      }, id
+    ],
+    (error, results) => {
+      if (error) {
+        console.log(error);
+      } else {
+        res.redirect("/");
+      }
+    }
+  );
+};
--- router.js
+++ router.js
@@ -14,11 +14,35 @@ router.get("/", (req, res) => {
 });
 
 //RUTA PARA CREAR USUARIOS
-router.get('/create', (req, res)=>{
-    res.render('create');
-})
+router.get("/create", (req, res) => {
+  res.render("create");
+});
 
-const crud = require('./controllers/crud');
-router.post('/save', crud.save)
+//Ruta para editar registro
+router.get("/edit/:id", (req, res) => {
+  const id = req.params.id;
+  conexion.query("SELECT * FROM users WHERE id=?", [id], (error, results) => {
+    if (error) {
+      throw error;
+    } else {
+      res.render("edit", { user: results[0] });
+    }
+  });
+});
+
+router.get('/delete/:id', (req, res)=> {
+    const id = req.params.id
+    conexion.query('DELETE FROM users WHERE id = ?', [id], (error, results)=>{
+        if (error) {
+            throw error;
+          } else {
+            res.redirect('/');
+          }
+    });
+});
+
+const crud = require("./controllers/crud");
+router.post("/save", crud.save);
+router.post("/update", crud.update);
 
 module.exports = router;
